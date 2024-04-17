01ef7f7edeafb47ed90a258593cb5f337fdf4e65
Akhil <83132051+akhil626626@users.noreply.github.com>
2024-04-17 16:40:10 -0400

    Update WebapplicationApplication.java

--- src/main/java/com/akhil/webapplication/WebapplicationApplication.java
+++ src/main/java/com/akhil/webapplication/WebapplicationApplication.java
@@ -5,20 +5,29 @@ import com.akhil.webapplication.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/orders")
 public class WebapplicationApplication {
 @Autowired
 	private OrderRepo repo;
 
-
+    @GetMapping
 	public List<Order> fetchOrder(){
 
-		return repo.orderDeatils();
+		return repo.orderDeatils()
+				.stream()
+				.sorted(Comparator.comparing(Order::getPrice))
+				.collect(Collectors.toList());
+
 	}
 
 	public static void main(String[] args) {
